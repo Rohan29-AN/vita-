ce9f3ff70e2d5292ca3d1a4c4a89ad4a0bf4f760
Rohan <rohanrc101@gmail.com>
2024-12-29 20:57:57 +0300

    [FEAT] create all the necessary directories and files

--- todo.sh
+++ todo.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+echo "TODO LIST"
+TODO_DIR="$HOME/.todo"
+TASKS_FILE="$TODO_DIR/tasks.txt"
+
+#Check if TODO_DIR exists
+if [ ! -d "$TODO_DIR" ]; then
+    mkdir -p "$TODO_DIR"
+fi
+
+#Check if TASKS_FILE exists
+if [ ! -f "$TASKS_FILE" ]; then
+    touch "$TASKS_FILE"
+fi
